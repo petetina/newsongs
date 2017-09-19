02ad27b02f72df67ebf100b383506f720d282a27
petetina <antoine.petetin@gmail.com>
2017-09-19 16:44:33 +0200

    SDK Deezer : on peut se connecter avec deezer.

--- app/src/main/java/newsongs/fr/newsongs/BaseActivity.java
+++ app/src/main/java/newsongs/fr/newsongs/BaseActivity.java
@@ -1,20 +1,56 @@
 package newsongs.fr.newsongs;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.deezer.sdk.model.Permissions;
+import com.deezer.sdk.network.connect.DeezerConnect;
 
 public class BaseActivity extends AppCompatActivity {
+    public static final String DEEZER_API_KEY = "252962";
+    protected DeezerConnect mDeezerConnect = null;
+    protected static final String[] PERMISSIONS = new String[]{
+            Permissions.BASIC_ACCESS, Permissions.OFFLINE_ACCESS,
+            Permissions.MANAGE_LIBRARY,
+            Permissions.LISTENING_HISTORY
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.abs_layout);
+
+        mDeezerConnect = new DeezerConnect(this, DEEZER_API_KEY);
     }
 
     @Override
     public void onBackPressed() {
 
+    }
+
+    /**
+     * Handle errors by displaying a toast and logging.
+     *
+     * @param exception
+     *            the exception that occured while contacting Deezer services.
+     */
+    protected void handleError(final Exception exception) {
+        String message = exception.getMessage();
+        if (TextUtils.isEmpty(message)) {
+            message = exception.getClass().getName();
+        }
+
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        ((TextView) toast.getView().findViewById(android.R.id.message)).setTextColor(Color.RED);
+        toast.show();
+
+        Log.e("BaseActivity", "Exception occured " + exception.getClass().getName(), exception);
     }
 }
