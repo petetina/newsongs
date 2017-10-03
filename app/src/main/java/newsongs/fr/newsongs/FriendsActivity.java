68634f42647b4077b2bbe1672818425defc684c5
brognaa <GY5k5Ty9>
2017-10-03 15:00:54 +0200

    add customlist model

--- app/src/main/java/newsongs/fr/newsongs/FriendsActivity.java
+++ app/src/main/java/newsongs/fr/newsongs/FriendsActivity.java
@@ -2,12 +2,23 @@ package newsongs.fr.newsongs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FriendsActivity extends BaseActivity {
 
+    ListView lv;
+    ArrayList prgmName;
+
+    public static int [] prgmImages={R.drawable.background,R.drawable.background,R.drawable.background,R.drawable.background,R.drawable.background,R.drawable.background,R.drawable.background,R.drawable.background,R.drawable.background};
+    public static String [] prgmNameList={"Let Us C","c++","JAVA","Jsp","Microsoft .Net","Android","PHP","Jquery","JavaScript"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(new CustomAdapter(this, prgmNameList, prgmImages));
     }
 }
