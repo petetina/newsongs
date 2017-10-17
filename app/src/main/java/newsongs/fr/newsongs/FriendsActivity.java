08409e214d5d8a89d2c7e59b4a595b93655c0ba8
brognaa <GY5k5Ty9>
2017-10-17 15:37:28 +0200

    test api friends

--- app/src/main/java/newsongs/fr/newsongs/FriendsActivity.java
+++ app/src/main/java/newsongs/fr/newsongs/FriendsActivity.java
@@ -1,10 +1,23 @@
 package newsongs.fr.newsongs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import newsongs.fr.newsongs.API.ServiceGenerator;
+import newsongs.fr.newsongs.API.UtilisateurClient;
+import newsongs.fr.newsongs.Models.Reponse;
+import newsongs.fr.newsongs.Models.Utilisateur;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FriendsActivity extends BaseActivity {
 
@@ -12,13 +25,43 @@ public class FriendsActivity extends BaseActivity {
     ArrayList prgmName;
 
     public static int [] prgmImages={R.drawable.ic_delete_forever_black_24dp,R.drawable.ic_delete_forever_black_24dp,R.drawable.ic_delete_forever_black_24dp,R.drawable.ic_delete_forever_black_24dp,R.drawable.ic_delete_forever_black_24dp,R.drawable.ic_delete_forever_black_24dp,R.drawable.ic_delete_forever_black_24dp,R.drawable.ic_delete_forever_black_24dp,R.drawable.ic_delete_forever_black_24dp};
-    public static String [] prgmNameList={"Let Us C","c++","JAVA","Jsp","Microsoft .Net","Android","PHP","Jquery","JavaScript"};
+    public static String [] prgmNameList={"Gerard","Antoine","Monique","Jean-Celestin","Marie-therese","Michel","Marcelle","Christian","Bertrand"};
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
-
         lv = (ListView) findViewById(R.id.listView);
         lv.setAdapter(new CustomAdapter(this, prgmNameList, prgmImages));
+
+        getFriendsByUser();
+    }
+
+
+
+    private void getFriendsByUser(){
+
+        SharedPreferences settings = getApplicationContext().getSharedPreferences(Tools.PREFS_NAME, Context.MODE_PRIVATE); //1
+        int idutilisateur = settings.getInt("idutilisateur", -2); //2//Appel à notre API pour créer l'utilisateur
+
+        UtilisateurClient service = ServiceGenerator.createService(UtilisateurClient.class);
+
+        Call<Utilisateur> user = service.findById(2);
+
+        user.enqueue(new Callback<Utilisateur>() {
+
+            @Override
+            public void onResponse(Call<Utilisateur> call, Response<Utilisateur> response) {
+                Log.e("c","coucou2");
+                Toast.makeText(getApplicationContext(), response.body().getIdutilisateur() + "", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<Utilisateur> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "pas ok", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
